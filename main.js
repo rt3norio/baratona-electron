c135e52c88727db24397772821eb7d3a24ea6b4a
Rodrigo Tenorio <rodrigo.tenorio@funcionalcorp.com.br>
2019-05-30 11:27:45 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,42 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, BrowserView} = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({ width: 800, height: 600 })
+  win.on('closed', () => {
+    win = null
+  })
+  
+  let view = new BrowserView()
+  win.setBrowserView(view)
+  view.setBounds({ x: 0, y: 0, width: 800, height: 600 })
+  view.webContents.loadURL('https://electronjs.org')
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+
