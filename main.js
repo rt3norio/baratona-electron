e2a5e14850260b71661e778dc8afaa8ee6abe719
Renato Flizzola <renato.filizzola@fidelize.com.br>
2019-05-30 17:40:34 -0300

    #baratona Consertando bug variavel nao existe

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ log.info('App starting...');
 
 function sendStatusToWindow(text) {
     log.info(text);
-    win.webContents.send('message', text);
+    mainWindow.webContents.send('message', text);
   }
 
 function createWindow () {
